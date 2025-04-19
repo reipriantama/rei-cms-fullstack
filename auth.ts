91fbb059879664a537816c90902e1604b913ab42
reiprntm <priantareinato@gmail.com>
2025-04-19 21:55:02 +0700

    fix: Correct import path for User type in auth.ts

--- auth.ts
+++ auth.ts
@@ -2,7 +2,7 @@ import NextAuth from "next-auth";
 import { authConfig } from "./auth.config";
 import Credentials from "next-auth/providers/credentials";
 import { z } from "zod";
-import type { User } from "@/app/lib/definitions";
+import type { User } from "@/app/db/definitions";
 import bcrypt from "bcryptjs";
 import postgres from "postgres";
 
