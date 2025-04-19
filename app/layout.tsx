533bd07d2e74bd3bbe9f23ace5355bda356de872
reiprntm <priantareinato@gmail.com>
2025-04-19 20:51:01 +0700

    feat: Add metadata for pages including Customers, Create Invoice, Edit Invoice, Invoices, and Login

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,14 @@
-import '@/app/ui/global.css';
-import { inter } from '@/app/ui/fonts';
- 
+import "@/app/ui/global.css";
+import { inter } from "@/app/ui/fonts";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: {
+    template: "%s | Acme Dashboard",
+    default: "Acme Dashboard",
+  },
+};
+
 export default function RootLayout({
   children,
 }: {
