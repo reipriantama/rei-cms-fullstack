857acec3ec473e6831d5fd03f9ac81bb5c19bb77
reiprntm <priantareinato@gmail.com>
2025-04-19 20:00:55 +0700

    feat: Add error handling for invoice creation and update; log errors in fetchInvoiceById

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -147,10 +147,9 @@ export async function fetchInvoiceById(id: string) {
 
     const invoice = data.map((invoice) => ({
       ...invoice,
-      // Convert amount from cents to dollars
       amount: invoice.amount / 100,
     }));
-
+    console.log(invoice);
     return invoice[0];
   } catch (error) {
     console.error("Database Error:", error);
